8dd19a39ec412b9b19f27366d7b2a3274a424eb8
Vladimir Kulich <vova011004@gmail.com>
2023-08-22 21:19:41 +0400

    feat: added POST request for CurrenciesServlet

--- src/main/java/models/Currency.java
+++ src/main/java/models/Currency.java
@@ -15,4 +15,12 @@ public class Currency {
     private String fullName;
     @NonNull
     private String sign;
+
+    @Override
+    public String toString() {
+        return "\n"+"\t{\n" + "\t\t\"id\": " + id + ", \n" +
+                "\t\t\"name\": " + "\"" + fullName + "\", \n" +
+                "\t\t\"code\": " + "\"" + code + "\", \n" +
+                "\t\t\"sign\": " + "\"" + sign + "\"\n"+"\t}";
+    }
 }
